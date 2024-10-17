127071ade4b1bd2b083bf7b4d693140062d8db64
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-17 17:38:20 +0200

    fix loop for

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,29 +8,33 @@ const btnGenerateEL = document.querySelector('button')
   
 btnGenerateEL.addEventListener('click', function(){
 
+  for(let i = 0; i < 10; i++){
+
   axios("https://flynn.boolean.careers/exercises/api/random/mail")
   .then(response =>{
     //richiesta al server e la stampo
     console.log(response);
   
    //inserisco in una variabile la risposta del server
-    const mail = response.data.response;
-  
-    console.log(mail);
+   
+   const mail = response.data.response;
 
+   
+   
 
   //creo un elemento lista tramite createelement  
 
-  for(let i = 0; i < 10; i++){
+  //for(let i = 0; i < 10; i++){
   const listEl = document.createElement("li")
 
   listEl.innerText = `new mail: ${mail}`
 
   //console.log(listEl)
   mailListEL.appendChild(listEl)
+})
 }
   
-})
+
 
 })
 
